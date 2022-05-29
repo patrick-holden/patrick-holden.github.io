f119af48cf6f79e6f5001b0141ccff36268dd8d7
patrick-holden <holden.patrick@gmail.com>
2022-05-29 16:38:37 +0100

    add JS scripts and rebuild to in-progress branch

---
+++ js/scripts.js
@@ -0,0 +1,103 @@
+function showText1(){
+  document.getElementById("hover-wine").innerHTML=
+    '<div class="overlay">' +
+    '<div class="text-flex">' +
+    '<div>' +
+    '<h3 class="project-title">\'VinoVero\'</h3>' +
+    '<p class="project-text">A Wine Collection App Built With PHP</p>' +
+    '</div>' +
+    '<div>' +
+    '<p class="project-desc">Another div with more detailed content in it.</p>' +
+    '</div>' +
+    '<div>' +
+    '<h4>Competencies:</h4>' +
+    '<ul class="project-competencies">' +
+    '<li>A competency</li>' +
+    '<li>And another</li>' +
+    '<li>And a last</li>' +
+    '</ul>' +
+    '</div>' +
+    '</div>' +
+    '</div>';
+}
+function hide1(){
+  document.getElementById("hover-wine").innerHTML="";
+}
+
+function showText2(){
+  document.getElementById("hover-carpe").innerHTML=
+    '<div class="overlay">' +
+    '<div class="text-flex">' +
+    '<div>' +
+    '<h3 class="project-title">\'CarPe - Diem\'</h3>' +
+    '<p class="project-text">An OOP Car Collection App</p>' +
+    '</div>' +
+    '<div>' +
+    '<p class="project-desc">Another div with more detailed content in it.</p>' +
+    '</div>' +
+    '<div>' +
+    '<h4>Competencies:</h4>' +
+    '<ul class="project-competencies">' +
+    '<li>A competency</li>' +
+    '<li>And another</li>' +
+    '<li>And a last</li>' +
+    '</ul>' +
+    '</div>' +
+    '</div>' +
+    '</div>';
+}
+function hide2(){
+  document.getElementById("hover-carpe").innerHTML="";
+}
+
+function showText3(){
+  document.getElementById("hover-scran").innerHTML=
+    '<div class="overlay">' +
+    '<div class="text-flex">' +
+    '<div>' +
+    '<h3 class="project-title">\'ScranMaster\'</h3>' +
+    '<p class="project-text">A Food Delivery App (react.js)</p>' +
+    '</div>' +
+    '<div>' +
+    '<p class="project-desc">Another div with more detailed content in it.</p>' +
+    '</div>' +
+    '<div>' +
+    '<h4>Competencies:</h4>' +
+    '<ul class="project-competencies">' +
+    '<li>A competency</li>' +
+    '<li>And another</li>' +
+    '<li>And a last</li>' +
+    '</ul>' +
+    '</div>' +
+    '</div>' +
+    '</div>';
+}
+function hide3(){
+  document.getElementById("hover-scran").innerHTML="";
+}
+
+function showText4(){
+  document.getElementById("hover-wordrunner").innerHTML=
+    '<div class="overlay">' +
+    '<div class="text-flex">' +
+    '<div>' +
+    '<h3 class="project-title">\'WordRunner\'</h3>' +
+    '<p class="project-text">JavaScript Wordguessing Game</p>' +
+    '</div>' +
+    '<div>' +
+    '<p class="project-desc">Another div with more detailed content in it.</p>' +
+    '</div>' +
+    '<div>' +
+    '<h4>Competencies:</h4>' +
+    '<ul class="project-competencies">' +
+    '<li>A competency</li>' +
+    '<li>And another</li>' +
+    '<li>And a last</li>' +
+    '</ul>' +
+    '</div>' +
+    '</div>' +
+    '</div>';
+}
+function hide4(){
+  document.getElementById("hover-wordrunner").innerHTML="";
+}
