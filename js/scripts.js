9925946967fa5c437115db44c4fc55633a9a16c1
patrick-holden <holden.patrick@gmail.com>
2022-06-06 22:43:41 +0100

    font sizing

--- js/scripts.js
+++ js/scripts.js
@@ -20,28 +20,29 @@ function script(){
 
 function showText1(){
   document.getElementById("hover-wine").innerHTML =
-  '<div class="overlay">' +
+    '<div class="overlay">' +
       '<div class="text-flex">' +
-      '<div>' +
-        '<h4 class="project-title">\'VinoVero\'</h4>' +
-        '<p class="project-summary">A Wine Collection App Built With PHP</p>' +
+        '<div>' +
+          '<h4 class="project-title">\'VinoVero\'</h4>' +
+          '<p class="project-summary">A Wine Collection App Built With PHP</p>' +
+        '</div>' +
+        '<div>' +
+          '<p class="project-desc">My first site built using PHP, with a 7 table many-to-many MySQL database on the back-end; a simple insert form for new entries is validated against malicious injection.</p>' +
+        '</div>' +
+        '<div class="github-live-links">' +
+          '<a href="https://github.com/patrick-holden/collection" target="_blank">' +
+            '<img src="./Images/GitHub-Mark-Light-120px-plus.png" class="github-live-logo-sml" alt="A link to my projects listed GitHub">' +
+          '</a>' +
+        '</div>'+
+        '<div class="project-competencies">' +
+          '<h4>Competencies:</h4>' +
+          '<ul class="project-competencies-list">' +
+            '<li>PHP</li>' +
+            '<li>MySQL</li>' +
+          '</ul>' +
+        '</div>' +
       '</div>' +
-      '<div>' +
-      '<p class="project-desc">My first site built using PHP, with a 7 table many-to-many MySQL database on the back-end; a simple insert form for new entries is validated against malicious injection.</p>' +
-      '</div>' +
-      '<div class="github-live-links">' +
-        '<a href="https://github.com/patrick-holden/collection" target="_blank">' +
-          '<img src="./Images/GitHub-Mark-Light-120px-plus.png" class="github-live-logo-sml" alt="A link to my projects listed GitHub">' +
-        '</a>' +
-      '</div>'+
-      '<div class="project-competencies">' +
-      '<h4>Competencies:</h4>' +
-      '<ul class="project-competencies-list">' +
-      '<li>PHP</li>' +
-      '<li>MySQL</li>' +
-      '</ul>' +
-      '</div>' +
-      '</div>';
+    '</div>';
 }
 
 function hide1(){
@@ -53,35 +54,35 @@ function hide1(){
 function showText2(){
   document.getElementById("hover-carpe").innerHTML=
     '<div class="overlay">' +
-    '<div class="text-flex">' +
-    '<div>' +
-      '<h4 class="project-title">\'CarPe - Diem\'</h4>' +
-      '<p class="project-summary">An OOP Car Collection App</p>' +
-    '</div>' +
-    '<div>' +
-    '<p class="project-desc">Built in OOP, with hydrators, the singleton pattern, and a Data Access Layer, I added a search function, allowing partial searches and protecting against malicious injection via both Regex and prepared statements.</p>' +
-    '</div>' +
-    '<div class="github-live-links">' +
-      '<div class="github-links">' +
-        '<a href="https://github.com/iO-Academy/2022-mar-carcollection" target="_blank">' +
-          '<img src="./Images/GitHub-Mark-Light-120px-plus.png" class="github-live-logo-sml" alt="A link to my projects listed GitHub">' +
-        '</a>' +
+      '<div class="text-flex">' +
+        '<div>' +
+          '<h4 class="project-title">\'CarPe - Diem\'</h4>' +
+          '<p class="project-summary">An OOP Car Collection App</p>' +
+        '</div>' +
+        '<div>' +
+          '<p class="project-desc">Built in OOP, with hydrators, the singleton pattern, and a Data Access Layer, I added a search function, allowing partial searches and protecting against malicious injection via both Regex and prepared statements.</p>' +
+        '</div>' +
+        '<div class="github-live-links">' +
+          '<div class="github-links">' +
+            '<a href="https://github.com/iO-Academy/2022-mar-carcollection" target="_blank">' +
+              '<img src="./Images/GitHub-Mark-Light-120px-plus.png" class="github-live-logo-sml" alt="A link to my projects listed GitHub">' +
+            '</a>' +
+          '</div>' +
+          '<div class="live-links">' +
+            '<a href="https://dev.io-academy.uk/projects/2022-march/2022-mar-carcollection/index.php" target="_blank">' +
+              '<img src="./Images/white-live-button.png" class="live-site-logo-sml" alt="A link to the live hosting of my car collection OOP build, CarPe-Diem">' +
+            '</a>' +
+          '</div>' +
+        '</div>' +
+        '<div class="project-competencies">' +
+          '<h4>Competencies:</h4>' +
+          '<ul class="project-competencies-list">' +
+            '<li>PHP</li>' +
+            '<li>SOLID</li>' +
+            '<li>MySQL</li>' +
+          '</ul>' +
+        '</div>' +
       '</div>' +
-      '<div class="live-links">' +
-        '<a href="https://dev.io-academy.uk/projects/2022-march/2022-mar-carcollection/index.php" target="_blank">' +
-          '<img src="./Images/white-live-button.png" class="live-site-logo-sml" alt="A link to the live hosting of my car collection OOP build, CarPe-Diem">' +
-        '</a>' +
-      '</div>' +
-    '</div>' +
-    '<div class="project-competencies">' +
-    '<h4>Competencies:</h4>' +
-    '<ul class="project-competencies-list">' +
-    '<li>PHP</li>' +
-    '<li>SOLID</li>' +
-    '<li>MySQL</li>' +
-    '</ul>' +
-    '</div>' +
-    '</div>' +
     '</div>';
 }
 
@@ -94,35 +95,35 @@ function hide2(){
 function showText3(){
   document.getElementById("hover-scran").innerHTML=
     '<div class="overlay">' +
-    '<div class="text-flex">' +
-    '<div>' +
-      '<h4 class="project-title">\'ScranMaster\'</h4>' +
-      '<p class="project-summary">A Food Delivery App (react.js)</p>' +
-    '</div>' +
-    '<div>' +
-    '<p class="project-desc">A bootstrap-styled food delivery app, created in react, using an asynchronous fetch to access an API, object destructing to distribute through the component tree, handling orders via POST request.</p>' +
-    '</div>' +
-    '<div class="github-live-links">' +
-      '<div class="github-links">' +
-        '<a href="https://github.com/iO-Academy/2022-mar-scranmaster" target="_blank">' +
-          '<img src="./Images/GitHub-Mark-Light-120px-plus.png" class="github-live-logo-sml" alt="A link to my projects listed GitHub">' +
-        '</a>' +
-      '</div>'+
-      '<div class="live-links">' +
-        '<a href="https://2022-mar-scranmaster.dev.io-academy.uk/" target="_blank">' +
-          '<img src="./Images/white-live-button.png" class="live-site-logo-sml" alt="A link to the live hosting of my Food Delivery build in react.js, which we called Scran-Master, internally">' +
-        '</a>' +
+      '<div class="text-flex">' +
+        '<div>' +
+          '<h4 class="project-title">\'ScranMaster\'</h4>' +
+          '<p class="project-summary">A Food Delivery App (react.js)</p>' +
+        '</div>' +
+        '<div>' +
+          '<p class="project-desc">A bootstrap-styled food delivery app, created in react, using an asynchronous fetch to access an API, object destructing to distribute through the component tree, handling orders via POST request.</p>' +
+        '</div>' +
+        '<div class="github-live-links">' +
+          '<div class="github-links">' +
+            '<a href="https://github.com/iO-Academy/2022-mar-scranmaster" target="_blank">' +
+              '<img src="./Images/GitHub-Mark-Light-120px-plus.png" class="github-live-logo-sml" alt="A link to my projects listed GitHub">' +
+            '</a>' +
+          '</div>'+
+          '<div class="live-links">' +
+            '<a href="https://2022-mar-scranmaster.dev.io-academy.uk/" target="_blank">' +
+              '<img src="./Images/white-live-button.png" class="live-site-logo-sml" alt="A link to the live hosting of my Food Delivery build in react.js, which we called Scran-Master, internally">' +
+            '</a>' +
+          '</div>' +
+        '</div>' +
+        '<div class="project-competencies">' +
+          '<h4>Competencies:</h4>' +
+          '<ul class="project-competencies-list">' +
+            '<li>React.js</li>' +
+            '<li>Bootstrap</li>' +
+            '<li>Postman</li>' +
+          '</ul>' +
+        '</div>' +
       '</div>' +
-    '</div>' +
-    '<div class="project-competencies">' +
-    '<h4>Competencies:</h4>' +
-    '<ul class="project-competencies-list">' +
-    '<li>React.js</li>' +
-    '<li>Bootstrap</li>' +
-    '<li>Postman</li>' +
-    '</ul>' +
-    '</div>' +
-    '</div>' +
     '</div>';
 }
 
@@ -135,35 +136,35 @@ function hide3(){
 function showText4(){
   document.getElementById("hover-wordrunner").innerHTML=
     '<div class="overlay">' +
-    '<div class="text-flex">' +
-    '<div>' +
-      '<h4 class="project-title">\'WordRunner\'</h4>' +
-      '<p class="project-summary">JavaScript Wordguessing Game</p>' +
-    '</div>' +
-    '<div>' +
-    '<p class="project-desc">A simple game made with vanilla JavaScript, and built in a six person scrum team; the app uses a JSON fetch request, to display synonyms so players can attempt to guess the next word.</p>' +
-    '</div>' +
-    '<div class="github-live-links">' +
-      '<div class="github-links">' +
-        '<a href="https://github.com/iO-Academy/2022-mar-word-finder" target="_blank">' +
-          '<img src="./Images/GitHub-Mark-Light-120px-plus.png" class="github-live-logo-sml" alt="A link to my projects listed GitHub">' +
-        '</a>' +
+      '<div class="text-flex">' +
+        '<div>' +
+          '<h4 class="project-title">\'WordRunner\'</h4>' +
+          '<p class="project-summary">JavaScript Wordguessing Game</p>' +
+        '</div>' +
+        '<div>' +
+          '<p class="project-desc">A simple game made with vanilla JavaScript, and built in a six person scrum team; the app uses a JSON fetch request, to display synonyms so players can attempt to guess the next word.</p>' +
+        '</div>' +
+        '<div class="github-live-links">' +
+          '<div class="github-links">' +
+            '<a href="https://github.com/iO-Academy/2022-mar-word-finder" target="_blank">' +
+              '<img src="./Images/GitHub-Mark-Light-120px-plus.png" class="github-live-logo-sml" alt="A link to my projects listed GitHub">' +
+            '</a>' +
+          '</div>' +
+          '<div class="live-links">' +
+            '<a href="https://dev.io-academy.uk/projects/2022-march/wordrunner/" target="_blank">' +
+              '<img src="./Images/white-live-button.png" class="live-site-logo-sml" alt="A link to the live hosting of my Food Delivery build in react.js, which we called Scran-Master, internally">' +
+            '</a>' +
+          '</div>' +
+        '</div>' +
+        '<div class="project-competencies">' +
+          '<h4>Competencies:</h4>' +
+          '<ul class="project-competencies-list">' +
+            '<li>JavaScript</li>' +
+            '<li>CSS</li>' +
+            '<li>HTML</li>' +
+          '</ul>' +
+        '</div>' +
       '</div>' +
-      '<div class="live-links">' +
-        '<a href="https://dev.io-academy.uk/projects/2022-march/wordrunner/" target="_blank">' +
-          '<img src="./Images/white-live-button.png" class="live-site-logo-sml" alt="A link to the live hosting of my Food Delivery build in react.js, which we called Scran-Master, internally">' +
-        '</a>' +
-      '</div>' +
-    '</div>' +
-    '<div class="project-competencies">' +
-    '<h4>Competencies:</h4>' +
-    '<ul class="project-competencies-list">' +
-    '<li>JavaScript</li>' +
-    '<li>CSS</li>' +
-    '<li>HTML</li>' +
-    '</ul>' +
-    '</div>' +
-    '</div>' +
     '</div>';
 }
 
